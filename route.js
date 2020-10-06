8e5a8971c7f5367539942d594f5af7093f74182f
kavyesh.shah <kavyesh.shah@openxcellinc.com>
2020-10-06 23:25:20 +0530

    fix: var name

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var hellomod1 = require('./controllers/hellomode2')
+var hellomod2 = require('./controllers/hellomode2')
 
 router.get('/', hellomod2.helloModule);
 
